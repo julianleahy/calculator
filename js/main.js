742f71d8591c958d694341d38ce183ec8bf09e49
julianleahy <julianleahynz@gmail.com>
2018-03-14 09:08:58 +1300

    add method to handle when numerical buttons clicked

--- js/main.js
+++ js/main.js
@@ -26,8 +26,35 @@ $(function () {
       $('.output').html(current);
       $('.chain').html(chain);
     }
+
+    // return last value in chain or 0 if empty
+    lastValueEntered() {
+      return (this._chain.length > 0) ? this._chain[this._chain.length - 1] : 0;
+    }
   } //= End Calculator Class
 
   // instantiate new calculator object
   const c = new Calculator();
+
+  // number pad clicked
+  $('.numerical').on('click', function () {
+    // if last value in chain is an operator clear the current array ready to display next number/s
+    if (isNaN(c.lastValueEntered()) && c.lastValueEntered() != '.') c._current = [];
+
+    // get value of pressed button
+    const numb = $(this).text();
+
+    // prevent multiple 0's at beginning
+    if (numb === '0' && c._chain.length === 0) return;
+
+    // check number only has one period
+    if (numb === '.' && c.current.includes('.')) return;
+
+    // update arrays
+    c.current = numb;
+    c.chain = numb;
+
+    c.displayOutput(c.current, c.chain);
+
+  })
 })
